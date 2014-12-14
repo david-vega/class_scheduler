c97ed82bf44ecf05b010be419ebf4ce208cdd34f
David Vega <zetacu@gmail.com>
2014-12-13 18:12:57 -0800

    set headers and turn layouts into haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    get '/signup', to: 'devise/registrations#new'
+    delete '/logout', to: 'devise/sessions#destroy'
+  end
+
+  root to: 'class_scheduler#index'
 end
