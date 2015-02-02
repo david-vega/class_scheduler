112a8720469e58ce2d1d311b7adfe53cc741b405
David Vega <zetacu@gmail.com>
2015-02-01 22:40:59 -0800

    include start/end methods in decorator spec

--- spec/decorators/reservation_decorator_spec.rb
+++ spec/decorators/reservation_decorator_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 describe ReservationDecorator do
   let(:reservation){ FactoryGirl.create :reservation }
+  let(:date_format){ '%Y/%m/%d %H:%M' }
   subject{ reservation.decorate }
 
   describe '#user_email' do
     it{ expect( subject.user_email ).to eql(reservation.user.email) }
+  end
+
+  describe '#start' do
+    it{ expect( subject.start ).to eql(reservation.start_time.strftime(date_format)) }
+  end
+
+  describe '#end' do
+    it{ expect( subject.end ).to eql(reservation.end_time.strftime(date_format)) }
   end
 end
