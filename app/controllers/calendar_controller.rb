cfd0abc0c27cf575f8946e19195cabc41b799ab5
David Vega <zetacu@gmail.com>
2015-03-29 23:11:02 -0700

    show reservations on day they start

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,5 +3,6 @@ class CalendarController < ApplicationController
 
   def show
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @reservations = Reservation.where(classroom_id: 1).starts_this_month(@date)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def calendar(date = Date.today, &block)
+  def calendar date = Date.today, &block
     Calendar.new(self, date, block).table
+  end
+
+  def reservations_today date
+    @reservations.where(start_time: (date.beginning_of_day..date.end_of_day))
   end
 end
